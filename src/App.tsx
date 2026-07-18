f822a7ccf3e13b0d487380b5771cb1000eb92606
Farid Sakhizad <farid.sakhyzad@gmail.com>
2026-07-17 21:58:48 -0600

    Alpha | .net configuration

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
       <header className="header">
         <div className="container container_header">
           <a href="/" className="logo">
-            <span className="logo-name">{SITE_NAME}</span>
+            <span className="logo-name">{SITE_NAME} | Stage</span>
             <span className="logo-pitch">Understand Time Across the World</span>
           </a>
 
